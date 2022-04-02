15b5bfe3996f0277e76e0441c648fa39a578584c
Felipe <machadofelipe2016@outlook.com>
2022-04-02 17:16:16 -0300

    c0-modify and rearrange all code

--- calcu/App.js
+++ calcu/App.js
@@ -1,15 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { render } from 'react-dom';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component{
-  
-  render(){ 
+    constructor(props){
+      super(props)
+      this.state ={
+        display:'',
+        result:''
+      }
+    } 
+     
+   handleOp(op){
+     if(op === 'C'){
+       this.setState({
+         display:"",
+         result:''
+       })
+     }else if(op ==='='){
+       this.setState({
+         display: this.state.result,
+         result: ''
+       })
+        
+     }else{
+      const display  = this.state.display +op
+      let result = this.state.result
+        
+      try{
+        let Fixedopera = display.split('x').join('*')
+          Fixedopera =Fixedopera.split('÷').join('/')
+          Fixedopera =Fixedopera.split(',').join('.')
+         
+        result = new String(eval(Fixedopera)).toString()    
+      }catch(e){} 
 
+      this.setState({
+        display,
+        result
+      })
+
+     }
+   }
+
+     
+  render(){ 
+   const col1Buttons= [
+     ['7','8','9'],
+     ['6','5','4'],
+     ['3','2','1'],
+     [',','0','='],
+   ]
+    
+   const Buttons = ['C','÷','x','+','-']
+     
     return (
       <View style={styles.container}>
-      <Text>Let's starte here now</Text>
+      <Text style={styles.display}> {this.state.display} </Text>
+      <Text style={styles.result} > {this.state.result} </Text>
+        
+        <View style={styles.buttons}>
+          
+          <View style={styles.col1} >
+            
+            {col1Buttons.map( (line,ind) => 
+              <View key={ind} style={styles.line}> 
+               { line.map ( op =>  <TouchableOpacity   key={op} style={styles.btn} onPress={() => this.handleOp(op)} > 
+                <Text style={styles.btnText}>
+                 {op} 
+                 </Text>
+                </TouchableOpacity> )}
+
+               </View> 
+               )}
+         
+          </View>
+          
+          <View style={styles.col2} >
+          { Buttons.map ( op =>  <TouchableOpacity key={op} style={styles.btn}  onPress={()=> this.handleOp(op)} > 
+            <Text style={styles.btnText}> 
+              {op}
+             </Text>
+            </TouchableOpacity> )}
+
+          </View>
+        
+        </View>
     
     </View>
   );
@@ -25,7 +102,7 @@ const styles = StyleSheet.create({
   },
   display:{
    flex:1,
-   backgroundColor:'#EFEFEF',
+   backgroundColor:'#fffFEF',
    fontSize:70,
    textAlign:'right',
    paddingTop:30,
@@ -43,7 +120,7 @@ const styles = StyleSheet.create({
   
   buttons:{
   flex:5,
-  flexDirection:'row'
+  flexDirection:'row',
   },
   
   line:{
@@ -57,15 +134,18 @@ const styles = StyleSheet.create({
    btnText:{
      textAlign:'center',
      fontSize:50,
-     justifyContent:'center'
+     justifyContent:'center',
+     color:'white'
    },
 
   col1:{
     flex:3,
-    backgroundColor:'grey'
+    backgroundColor:'black'
   },
   col2:{
     flex:1,
-    backgroundColor:'#1EF4A2'
+    backgroundColor:'blue',
+    opacity:0.7
+   
   }
 });
